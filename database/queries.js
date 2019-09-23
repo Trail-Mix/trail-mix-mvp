d8d0ca9869c1277ea16fc4947b658cb3df4c86f2
Ke Chen <keke@Kes-MacBook-Air.local>
2019-09-22 18:15:26 -0700

    login/signup/

--- database/queries.js
+++ database/queries.js
@@ -1,18 +1,57 @@
 const { Pool } = require('pg');
 const path = require('path')
 const pool = new Pool({
-  connectionString:	'postgres://hiiudcpk:Ozq8Ezo4e1c0HZGONL6i2M5_PmHDKUMV@salt.db.elephantsql.com:5432/hiiudcpk'
+  connectionString: 'postgres://hiiudcpk:Ozq8Ezo4e1c0HZGONL6i2M5_PmHDKUMV@salt.db.elephantsql.com:5432/hiiudcpk'
 });
 
 // query fetching all comments
 const getComment = (req, res, next) => {
-    pool.query('SELECT * FROM comments', (error, results) => {
-      if (error) throw error
-      res.locals.comment = results.rows
-      return next();
-    })
-  };
+  pool.query('SELECT * FROM comments', (error, results) => {
+    if (error) throw error
+    res.locals.comment = results.rows
+    return next();
+  })
+};
 
+
+// query username and password
+const verifyUser = (req, res, next) => {
+  const { username, password } = req.body;
+  pool.query('SELECT username, password FROM users',
+    //  where username = $1', [username],
+    (error, results) => {
+      // console.log("results==>", results.rows)
+      if (error) {
+        res.locals.error = { 'error': error };
+        return next(error)
+      }
+      // console.log(1)
+      res.locals.userinfo = results.rows;
+      // console.log('userInfo => ', res.locals.userinfo)
+      console.log('results.rows in queries.js:', res.locals.userinfo)
+      for (let obj of res.locals.userinfo) {
+        if (obj.username === username && obj.password === password) {
+          res.locals.verified = true;
+          return next();
+        }
+      }
+      res.locals.verified = false;
+    })
+}
+
+const createUser = (req, res, next) => {
+  const { username, password } = req.body;
+  console.log('username is:', username)
+  console.log('password is:', password)
+  pool.query('INSERT INTO users(username, password) VALUES ($1, $2) returning *', [username, password], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    console.log("results from create user => ", results.rows);
+    res.locals.createUser = results.rows;
+    return next();
+  })
+}
 module.exports = {
-    getComment
+  getComment, verifyUser, createUser
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const trailController = require('../controller/trailController');
+const queries = require('../database/queries.js')
 
 const app = express();
 const PORT = 3000;
@@ -10,38 +11,55 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 //sends homePage file upon entering home page
-app.get('/api', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../index.html'));
-  });
+app.get('/api/homepage', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+});
 
-  app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../index.html'));
-    // res.send(res.locals.trails);
-  })
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+  // res.send(res.locals.trails);
+})
+
 
 //   app.get('/data', trailController.getTrails, () => {
 //     res.send(res.locals.trails)
 //   })
 
+// post request for user infor
+app.post("/api/login", queries.verifyUser, (req, res) => {
+  const { verified } = res.locals;
+  console.log("verified in server ==> ", verified)
+
+  return res.status(200).json(verified);
+})
+
+// post request for user signup infor
+app.post("/api/signup", queries.createUser, (req, res) => {
+  const { createUser } = res.locals;
+  console.log("createUser from server ==>", createUser)
+  return res.json();
+})
+
 // catch-all route handler for any requests to an unknown route
 app.all('*', (req, res) => {
-    res.sendStatus(404);
-  });
+  res.sendStatus(404);
+});
 
 //global error handler
 app.use((err, req, res, next) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
-      status: 400,
-      message: { err: 'An error occurred' }, 
-    }
-    const errObj = Object.assign((defaultErr, err));
-    console.log(errObj.log);
-  
-    res.sendStatus(errObj.status).json(errorObj.message);
-  });
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  }
+  const errObj = Object.assign((defaultErr, err));
+  console.log(errObj.log);
+
+  res.sendStatus(errObj.status).json(errorObj.message);
+});
+
 
 //start server
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
-  });
+  console.log(`Server listening on port: ${PORT}`);
+});
